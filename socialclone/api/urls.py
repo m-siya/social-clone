48df8d9d7eadf5bc5a9f8417785c6a81236de2a4
Tanish Khatri <tanishkhatri03@gmail.com>
2023-05-26 19:22:55 +0530

    Added serializers

--- socialclone/api/urls.py
+++ socialclone/api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.getData) 
+    path('',views.getData), 
+    path('add/', views.addUser),
+    path('update/', views.updateData),
+    path('delete/', views.deleteUser)
 ]
--- socialclone/api/views.py
+++ socialclone/api/views.py
@@ -1,7 +1,31 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from social.models import User
+from .serializers import UserSerializer
 
 @api_view(['GET'])
 def getData(request):
-    person = {}
-    return Response(person)
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addUser(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateData(request):
+    users = User.objects.get(UserID=request.data['user_id_bin'])
+    serializer = UserSerializer(request, data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view
+def deleteUser(request):
+    users = User.objects.get(UserID = request.data)
+    users.delete()
+    return Response()
