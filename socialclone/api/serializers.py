b07f69acc26d4b7ab48f28f3c96e20fc70285677
Siya <del.siyamittal@gmail.com>
2023-05-26 22:25:38 +0530

    added more serializers

--- socialclone/api/serializers.py
+++ socialclone/api/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
-from social.models import User
+from social.models import User, Comment, Tag, Follower, Post
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('__all__')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('__all__')
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('__all__')
+
+class FollowerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follower
+        fields = ('__all__')
