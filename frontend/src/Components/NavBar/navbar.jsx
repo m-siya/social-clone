54de9b41c5fcb377d5f4d6df64c2aa45edba806e
Siya <del.siyamittal@gmail.com>
2023-07-24 22:56:36 +0530

    all functionality implemented

--- frontend/src/Components/NavBar/navbar.jsx
+++ frontend/src/Components/NavBar/navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import {BiSolidHome, BiSolidUser, BiSolidSearch, BiSolidLogOut, BiArrowFromLeft, BiArrowFromRight} from 'react-icons/bi';
 import "./navbar.scss"
 
 
@@ -12,25 +13,42 @@ const NavBar = () => {
     return (
         <div className={`navBar ${isNavOpen ? "open" : "closed"}`}>
             <button className="toggleButton" onClick={toggleNav}>
-                {isNavOpen ? "<<" : ">>"}
+                {isNavOpen ? <BiArrowFromRight/> : <BiArrowFromLeft/>}
             </button>
 
             <nav className="navLinks">
-                <a className="active" href="#home">
-                    Home
-                </a>
-                <a href="#profile"> 
-                    Profile
-                </a>
-                <a href="#search"> 
-                    Search
-                </a>
-                <a href="logout">
-                    Logout
-                </a>
-            </nav>
-            
-
+                {isNavOpen ? (
+                    <>
+                        <a className="active" href="#home">
+                            <BiSolidHome/> Home
+                        </a>
+                        <a href="#profile"> 
+                            <BiSolidUser/> Profile
+                        </a>
+                        <a href="#search"> 
+                            <BiSolidSearch/> Search
+                        </a>
+                        <a href="logout">
+                            <BiSolidLogOut/> Logout
+                        </a>
+                    </>
+                ) : (
+                    <>
+                        <a className="active" href="#home">
+                            <BiSolidHome/>
+                        </a>
+                        <a href="#profile"> 
+                            <BiSolidUser/>
+                        </a>
+                        <a href="#search"> 
+                            <BiSolidSearch/>
+                        </a>
+                        <a href="logout">
+                            <BiSolidLogOut/>
+                        </a>
+                    </>
+                )}               
+            </nav>           
         </div>
     )
 }
