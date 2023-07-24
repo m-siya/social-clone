0aef9650af7f454eb3530e57fc64407db406b15a
Siya <del.siyamittal@gmail.com>
2023-07-24 22:21:07 +0530

    working collapsible side nav bar

--- frontend/src/Components/NavBar/navbar.jsx
+++ frontend/src/Components/NavBar/navbar.jsx
@@ -1,45 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.scss"
-
-function openNav() {
-    document.getElementById("navBar").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("navBar").style.width = "0";
-}
 
 
 const NavBar = () => {
     const [isNavOpen, setIsNavOpen] = useState(false);
 
-    const openNav = () => {
-        setIsNavOpen(true);
-    };
-
-    const closeNav = () => {
-        setIsNavOpen(false);
-    };
-
     const toggleNav = () => {
-        setIsNavOpen((prevIsNavOpen) => !(prevIsNavOpen))
-    }
+        setIsNavOpen((prevIsNavOpen) => !prevIsNavOpen);
+    };
 
     return (
         <div className={`navBar ${isNavOpen ? "open" : "closed"}`}>
-            <a className="active" href="#home">
-                Home
-            </a>
-            <a href="#profile"> 
-                Profile
-            </a>
-            <a href="#search"> 
-                Search
-            </a>
-            <a href="logout">
-                Logout
-            </a>
+            <button className="toggleButton" onClick={toggleNav}>
+                {isNavOpen ? "<<" : ">>"}
+            </button>
+
+            <nav className="navLinks">
+                <a className="active" href="#home">
+                    Home
+                </a>
+                <a href="#profile"> 
+                    Profile
+                </a>
+                <a href="#search"> 
+                    Search
+                </a>
+                <a href="logout">
+                    Logout
+                </a>
+            </nav>
+            
 
         </div>
     )
