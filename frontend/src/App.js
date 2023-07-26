2538825f9e535e619c015239d3b687a4efb20391
Tanish Khatri <tanishkhatri03@gmail.com>
2023-07-26 21:12:37 +0530

    Implementation of Sign-up page and addition of links to buttons.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import Login from "./Pages/Login/login";
+import Signup from "./Pages/SignUp/signup";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path:"/signup",
+      element: <Signup/>,
+    },
+  ]);
+
   return (
     <div>
-      <Login/>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- frontend/src/Pages/SignUp/signup.jsx
+++ frontend/src/Pages/SignUp/signup.jsx
@@ -1,29 +1,31 @@
 import { Link } from "react-router-dom"
-import "./login.scss"
+import "./signup.scss"
 
-const Login = () => {
+const Signup = () => {
     return (
-        <div className="login">
+        <div className="signup">
             <div className="card1">
                 <div className="left">
-                    <h1>Bonjour.</h1>
+                    <h1>Beinvenido.</h1>
                     <p>
                         Placeholder paragraph text about 
                         the website or something else.
                     </p>
-                    <span>Don't have an account?</span>
-                    <Link to="/signup">
-                    <button>Sign Up</button>
+                    <span>Already have an account?</span>
+                    <Link to="/login">
+                    <button>Login</button>
                     </Link>
                 </div>
             </div>
             <div className = "card2">
                 <div className="right">
-                    <h1>Login</h1>
+                    <h1>Sign Up</h1>
                     <form>
                         <input type="text" placeholder="Username"/>
+                        <input type="email" placeholder="Email"/>
+                        <input type="text" placeholder="Username"/>
                         <input type="password" placeholder="Password"/>
-                        <button>Login</button>
+                        <button>Sign Up</button>
                     </form>
                 </div>
             </div>
@@ -31,4 +33,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default Signup
