983e3bca191b72f635d79c188ec175ac9cbc0135
Siya <del.siyamittal@gmail.com>
2023-07-26 14:18:57 +0530

    implemented comment

---
+++ frontend/src/Components/Comment/comment.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState, useRef} from "react";
+import "./comment.scss";
+
+const Comment = ({username, commentText, timeStamp}) => {
+    const commentRef = useRef(null);
+    const [commentHeight, setCommentHeight] = useState(null);
+    
+    useEffect(() => {
+        if (commentRef.current) {
+            const height = commentRef.current.scrollHeight + 97;
+            setCommentHeight(height);
+        }
+    }, [commentHeight]);
+
+    return (
+        <div className="comment" style={{ height: commentHeight}}>
+        <div className="commentHeader">
+            <div className="username">
+                {username}
+            </div>
+            <div className="timeStamp">
+                {timeStamp}
+            </div>
+        </div>
+        
+        <div className="commentText" ref={commentRef}>
+            {commentText}
+        </div>
+    </div>
+    )
+}
+
+export default Comment
